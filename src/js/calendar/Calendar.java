5f0968e0c627093d1f183586fbd49c07d596f342
강정석 <@DESKTOP-7NESOGV>
2021-04-13 13:01:41 +0900

    Calendar exercise

--- src/js/calendar/Calendar.java
+++ src/js/calendar/Calendar.java
@@ -1,9 +1,29 @@
 package js.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
+	private static final int[] MAX_DAYS = {32, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	private int getMaxDaysofMonth(int month) {
+		
+		return MAX_DAYS[month - 1 ];
+	}
+	
 	public static void main(String[] args) {
 	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("월을 입력하세요");
+		
+		int month = scanner.nextInt();
+		
+		Calendar cal = new Calendar();
+		
+		System.out.printf("%d월의 최대 일수는 %d입니다.", month, cal.getMaxDaysofMonth(month));
+		
+		scanner.close();
+		
 		
 		
 	}
